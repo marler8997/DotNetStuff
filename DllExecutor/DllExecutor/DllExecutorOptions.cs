155a51bbe9cc31bc66531148c30410970becde66
Jonathan Marler <marler8997@vandals.uidaho.edu>
2012-04-10 00:57:10 -0400

    Added DllExecutor

---
+++ DllExecutor/DllExecutor/DllExecutorOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Marler.OptionsParser;
+
+namespace Marler.DllExecutor
+{
+    public class DllExecutorOptions : Options
+    {
+        public DllExecutorOptions()
+        {
+        }
+
+        public override void PrintHeader()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
