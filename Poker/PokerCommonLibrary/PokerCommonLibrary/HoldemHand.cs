db9aac4ec8f8b205adebdaf78c9298b495fa2114
Jonathan Marler <jonathan.j.marler@hp.com>
2012-10-08 20:14:50 -0600

    Added Poker Library

---
+++ Poker/PokerCommonLibrary/PokerCommonLibrary/HoldemHand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Marler.Poker
+{
+    public class HoldemHand
+    {
+        enum HandState
+        {
+            PreFlop = 0,
+            Flop    = 1,
+            Turn    = 2,
+            River   = 3
+        }
+
+        private HandState handState;
+        
+        public HoldemHand()
+        {
+        }
+
+
+
+
+
+
+
+
+    }
+}
