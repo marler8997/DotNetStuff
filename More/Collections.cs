8d37dad77425d0e54041db8f1335010cdec02d92
Jonathan Marler <jonathan.j.marler@hp.com>
2016-05-03 11:00:05 -0600

    Added the OneOrMore list type

--- More/Collections.cs
+++ More/Collections.cs
@@ -74,4 +74,120 @@ namespace More
             get { return false; }
         }
     }
+    /// <summary>
+    /// A struct that contains one item, and a list of other items if there are any more.
+    /// This is used for lists of items, where there will typically be 1 item per entry, but
+    /// could have more then 1, and each entry will also never have 0 entries.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public struct OneOrMore<T> : IList<T>
+    {
+        public T first;
+        List<T> others;
+        public OneOrMore(T first)
+        {
+            this.first = first;
+            this.others = null;
+        }
+        public Int32 Count
+        {
+            get { return (others == null) ? 1 : 1 + others.Count; }
+        }
+        public void Add(T item)
+        {
+            if (others == null)
+            {
+                others = new List<T>();
+            }
+            others.Add(item);
+        }
+        public void Clear()
+        {
+            throw new NotSupportedException("Cannot clear an instance of OneOrMore, must always have at least one item");
+        }
+        public Boolean Contains(T item)
+        {
+            if (first.Equals(item))
+            {
+                return true;
+            }
+            return (others == null) ? false : others.Contains(item);
+        }
+        public void CopyTo(T[] array, Int32 arrayIndex)
+        {
+            array[arrayIndex] = first;
+            if (others != null)
+            {
+                others.CopyTo(array, arrayIndex + 1);
+            }
+        }
+        public Boolean IsReadOnly
+        {
+            get { return false; }
+        }
+        public bool Remove(T item)
+        {
+            if (item.Equals(first))
+            {
+                if (others == null || others.Count == 0)
+                {
+                    throw new InvalidOperationException("Cannot remove because this list must always have at least 1 item");
+                }
+                first = others[others.Count - 1];
+                others.RemoveAt(others.Count - 1);
+                return true;
+            }
+            return (others == null) ? false : others.Remove(item);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            yield return first;
+            if (others != null)
+            {
+                for (int i = 0; i < others.Count; i++)
+                {
+                    yield return others[i];
+                }
+            }
+        }
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public int IndexOf(T item)
+        {
+            if (item.Equals(first))
+            {
+                return 0;
+            }
+            return (others == null) ? -1 : others.IndexOf(item);
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return (index == 0) ? first : others[index - 1];
+            }
+            set
+            {
+                if (index == 0)
+                {
+                    first = value;
+                }
+                else
+                {
+                    others[index - 1] = value;
+                }
+            }
+        }
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
